9a7cbd8c998c3cb50396f7154acfd2a54f2289b0
Noppadol <nop_the_last_wizard@hotmail.com>
2025-06-29 13:20:49 +0700

    the project was build successfully on Render. Fixed gpt model in views.py for memory issue

--- tarot_chatbot/views.py
+++ tarot_chatbot/views.py
@@ -169,7 +169,8 @@ def tarot_chat(request):
 
 def ask_chatgpt(messages):
     response = client.chat.completions.create(
-        model="gpt-4",
+        # model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=messages,
         temperature=0.9,
     )
