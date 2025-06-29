b8f2c743eaa5f04e11667a4a7fe5dbd45aacfcf8
Noppadol <nop_the_last_wizard@hotmail.com>
2025-06-29 12:39:45 +0700

    updated settings.py

--- chatafinder_core/settings.py
+++ chatafinder_core/settings.py
@@ -28,10 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xz$4sm^l8qp*jcq@t$@43dwona_#al9x314p3=ma2=7&rr@%d!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 
@@ -122,11 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tarot_chatbot/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # used in deployment
+
+# Whitenoise for serving static files
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
