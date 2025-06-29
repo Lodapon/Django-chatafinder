4719db0dc88d122b74d15f98d0348a84e15661de
Noppadol <nop_the_last_wizard@hotmail.com>
2025-06-29 13:12:00 +0700

    using parameters from fortune_portal

--- render_build.sh
+++ render_build.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
-python manage.py collectstatic --noinput
+#!/usr/bin/env bash
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run database migrations
+python manage.py makemigrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
