c801b04ff141e0fc0112dcc892b5264ebc4df329
Noppadol <nop_the_last_wizard@hotmail.com>
2025-06-29 12:38:24 +0700

    Prepare for Render deployment

--- chatafinder_core/settings.py
+++ chatafinder_core/settings.py
@@ -122,12 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tarot_chatbot/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tarot_chatbot/templatetags/filters.py
+++ tarot_chatbot/templatetags/filters.py
@@ -4,5 +4,4 @@ register = template.Library()
 
 @register.filter
 def card_image_filename(card_name):
-    # Strip leading/trailing spaces and replace inner spaces with underscores
-    return card_name.strip().replace(' ', '_') + '.jpg'
+    return card_name.replace(' ', '_') + ".jpg"
